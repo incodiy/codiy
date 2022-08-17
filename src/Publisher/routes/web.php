4116f4d6800663c74d6a1a55454cff6c8ff8236f
wisnuwidi <wisnuwidi@gmail.com>
2022-08-17 10:19:56 +0700

    CoDIY Update - web routes

--- src/Publisher/routes/web.php
+++ src/Publisher/routes/web.php
@@ -14,11 +14,9 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function() {return view('welcome');});
 
-Route::get('/home', 'App\Http\Controllers\Front\Modules\HomeController@index')->name('home');
+Route::get('/home',        'App\Http\Controllers\Front\Modules\HomeController@index')->name('home');
 Route::get('/home/create', 'App\Http\Controllers\Front\Modules\HomeController@create')->name('home');
 Route::post('/home/store', 'App\Http\Controllers\Front\Modules\HomeController@store')->name('home');
 
@@ -26,11 +24,34 @@ Route::group(['middleware' => ['web']], function () {
 	
 	Auth::routes();
 	
-	Route::get	('/login',				['as' => 'login',			'uses' => 'App\Http\Controllers\Admin\System\AuthController@login']);
-	Route::post	('/login_processor',	['as' => 'login_processor',	'uses' => 'App\Http\Controllers\Admin\System\AuthController@login_processor']);
-	Route::get	('/logout',				['as' => 'logout',			'uses' => 'App\Http\Controllers\Admin\System\AuthController@logout']);
+	Route::get  ('/login',           ['as' => 'login',           'uses' => 'App\Http\Controllers\Admin\System\AuthController@login']);
+	Route::post ('/login_processor', ['as' => 'login_processor', 'uses' => 'App\Http\Controllers\Admin\System\AuthController@login_processor']);
+	Route::get  ('/logout',          ['as' => 'logout',          'uses' => 'App\Http\Controllers\Admin\System\AuthController@logout']);
 	
     Route::group(['middleware' => 'auth'], function() {
+    	
+    	Route::resource('dashboard', 'Admin\System\DashboardController');
+    	
+		// SYSTEM
+		Route::group(['prefix' => 'system'], function() {
+			
+			// CONFIGURATION
+			Route::group(['prefix' => 'config'], function() {
+				Route::resource('module',     'App\Http\Controllers\Admin\System\ModulesController',    ['as' => 'system.config']);
+				Route::resource('preference', 'App\Http\Controllers\Admin\System\PreferenceController', ['as' => 'system.config']);
+				Route::resource('group',      'App\Http\Controllers\Admin\System\GroupController',      ['as' => 'system.config']);/* 
+				Route::resource('icon',       'App\Http\Controllers\Admin\System\IconController',       ['as' => 'system.config']);
+				Route::resource('log',        'App\Http\Controllers\Admin\System\LogController',        ['as' => 'system.config']); */
+				
+			});
+			
+			// ACCOUNTS
+			Route::group(['prefix' => 'accounts'], function() {
+				Route::resource('user', 'App\Http\Controllers\Admin\System\UserController', ['as' => 'system.accounts']);
+			});
+			
+		});
+		
     	Route::group(['prefix' => 'modules'], function() {
     		Route::resource('form', 'App\Http\Controllers\Admin\Modules\FormController', ['as' => 'modules']);
     	});
