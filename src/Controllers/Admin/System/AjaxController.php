cd53a23df9adfe15e2f5cc1133a480dd816443e8
wisnuwidi <wisnuwidi@gmail.com>
2022-09-23 23:02:16 +0700

    Bismillah, Update System @:20220923

---
+++ src/Controllers/Admin/System/AjaxController.php
@@ -0,0 +1,27 @@
+<?php
+namespace Incodiy\Codiy\Controllers\Admin\System;
+
+use Incodiy\Codiy\Controllers\Core\Controller;
+
+/**
+ * Created on Sep 23, 2022
+ * 
+ * Time Created : 7:51:52 PM
+ *
+ * @filesource	AjaxController.php
+ *
+ * @author     wisnuwidi@gmail.com - 2022
+ * @copyright  wisnuwidi
+ * @email      wisnuwidi@gmail.com
+ */
+
+class AjaxController extends Controller {
+	
+	public function __construct() {
+		
+	}
+	
+	public function post() {
+		dd('process');
+	}
+}
