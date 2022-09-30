97e73ca5e864f41c4a94509ed407ea616b05fdfb
wisnuwidi <wisnuwidi@gmail.com>
2022-10-01 03:17:32 +0700

    Bismillah, Update System @:20221001

---
+++ src/Publisher/app/Http/Controllers/Admin/System/AjaxController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers\Admin\System;
+
+use Incodiy\Codiy\Controllers\Admin\System\AjaxController as Ajax;
+
+/**
+ * Created on Sep 23, 2022
+ * 
+ * Time Created : 7:51:52 PM
+ *
+ * @filesource	AjaxController.php
+ *
+ * @author     wisnuwidi@gmail.com - 2022
+ * @copyright  wisnuwidi
+ * @email      wisnuwidi@gmail.com
+ */
+
+class AjaxController extends Ajax {}
