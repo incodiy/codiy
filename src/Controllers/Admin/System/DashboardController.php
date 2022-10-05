9ff41998de045c94ec9d721a9dde07cf41434fc2
wisnuwidi <wisnuwidi@gmail.com>
2022-10-05 16:53:45 +0700

    Bismillah, Update System @:20221005

---
+++ src/Controllers/Admin/System/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+namespace Incodiy\Codiy\Controllers\Admin\System;
+
+use Incodiy\Codiy\Controllers\Core\Controller;
+
+/**
+ * Created on May 17, 2018
+ * Time Created	: 8:53:34 AM
+ * Filename		: DashboardController.php
+ *
+ * @filesource	DashboardController.php
+ *
+ * @author		wisnuwidi@gmail.com - 2018
+ * @copyright	wisnuwidi
+ * @email		wisnuwidi@gmail.com
+ */
+ 
+class DashboardController extends Controller {
+	
+	private $name			= 'article';
+	private $route_group	= 'modules.article';
+	public $table			= 'base_article';
+	
+	private $_hide_fields	= ['id'];
+	private $_set_tab		= [];
+	private $_tab_config	= [];
+	private $flag			= [];
+	
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function index() {
+		$this->setPage();
+		
+		return $this->render();
+	}
+}
--- src/Publisher/app/Http/Controllers/Admin/System/DashboardController.php
+++ src/Publisher/app/Http/Controllers/Admin/System/DashboardController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers\Admin\System;
 
-use Incodiy\Codiy\Controllers\Core\Controller;
+use Incodiy\Codiy\Controllers\Admin\System\DashboardController as Dashboard;
 
 /**
  * Created on May 17, 2018
@@ -14,33 +14,4 @@ use Incodiy\Codiy\Controllers\Core\Controller;
  * @copyright	wisnuwidi
  * @email		wisnuwidi@gmail.com
  */
- 
-class DashboardController extends Controller {
-	
-	private $name			= 'article';
-	private $route_group	= 'modules.article';
-	public $table			= 'base_article';
-	
-	private $_hide_fields	= ['id'];
-	private $_set_tab		= [];
-	private $_tab_config	= [];
-	private $flag			= [];
-	
-	public function __construct() {
-		parent::__construct();
-	}
-	
-	private function set_route($path) {
-		return "{$this->route_group}.{$path}";
-	}
-	
-	private function table_config() {
-		$this->form->table_hide_fields($this->table, $this->_hide_fields);
-	}
-	
-	public function index() {
-		$this->set_page(camel_case($this->name) . ' Lists', $this->name);
-		
-		return $this->render();
-	}
-}
+class DashboardController extends Dashboard {}
