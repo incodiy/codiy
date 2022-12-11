481fdae29f146080ec5f672512c1ff9663242467
wisnuwidi <wisnuwidi@gmail.com>
2022-12-11 22:17:17 +0700

    Bismillah, Update System @:20221211

---
+++ src/Publisher/app/Http/Controllers/Admin/System/ExtransloadController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers\Admin\System;
+use Incodiy\Codiy\Controllers\Admin\System\ExtransloadController as ETL;
+
+/**
+ * Created on Dec 10, 2022
+ * 
+ * Time Created : 1:15:44 AM
+ * Filename     : ExtransloadController.php
+ *
+ * @filesource ExtransloadController.php
+ *
+ * @author     wisnuwidi @Incodiy - 2022
+ * @copyright  wisnuwidi
+ * @email      incodiy@gmail.com
+ */
+ 
+class ExtransloadController extends ETL {}
