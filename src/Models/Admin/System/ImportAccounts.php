2c206ee0f97f69a2b5249a72dda1b85faffc0eb4
wisnuwidi <wisnuwidi@gmail.com>
2022-12-31 05:06:50 +0700

    Bismillah, Update System @:20221231

---
+++ src/Models/Admin/System/ImportAccounts.php
@@ -0,0 +1,15 @@
+<?php
+namespace Incodiy\Codiy\Models\Admin\System;
+/**
+ * Created on Dec 31, 2022
+ * 
+ * Time Created : 4:57:15 AM
+ * Filename     : ImportAccounts.php
+ *
+ * @filesource ImportAccounts.php
+ *
+ * @author     wisnuwidi @Incodiy - 2022
+ * @copyright  wisnuwidi
+ * @email      eclipsync@gmail.com
+ */
+class ImportAccounts extends User {}
