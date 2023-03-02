0b62e8ef69a7a8a09299f4cddeefe4bf30a1b98d
wisnuwidi <wisnuwidi@gmail.com>
2023-03-02 17:18:47 +0700

    Bismillah, Update System @:20230302

--- src/Publisher/public/assets/templates/default/js/scripts.js
+++ src/Publisher/public/assets/templates/default/js/scripts.js
@@ -29,44 +29,36 @@
 	$(window).ready(e), $(window).on("resize", e);
 	
 	$('#menu').metisMenu();
-	var slimScrollHeight = 98;
+	var sidebarSubHeight       = $('.sidebar-menu>.relative>.user-panel.light').outerHeight(true);
+	var sidebarHeadNSubHeight  = $('.sidebar-header').outerHeight(true) + sidebarSubHeight;
+	var slimScrollHeight       = sidebarHeadNSubHeight + 10;
+	
 	$('.menu-inner').slimScroll({
 		height : $(window).height() - slimScrollHeight,
-		maxHeight : '460px !important'
+		maxHeight : e + 'px !important'
 	});
 	$('.menu-inner').css("height", $(window).height() - slimScrollHeight);
 	$(window).resize(function() {
 		$('.menu-inner').css("height", $(window).height() - slimScrollHeight);
 	});
+	
 	$('.slimScrollDiv').css("height", $(window).height() - slimScrollHeight);
 	$(window).resize(function() {
 		$('.slimScrollDiv').css("height", $(window).height() - slimScrollHeight);
 	});
-	$('.slimScrollBar').css({
-		'right' : 'unset !important',
-		'left' : 0,
-	});
-	$('.scroolbox').slimScroll({
-		height : 'auto'
-	});
-	$('.chosen-drop').slimScroll({
-		height : 'auto'
-	});
-	$('.nofity-list').slimScroll({
-		height : '200px'
-	});
-	$('.timeline-area').slimScroll({
-		height : '500px'
-	});
-	$('.recent-activity').slimScroll({
-		height : 'calc(100vh - 114px)'
-	});
-	$('.settings-list').slimScroll({
-		height : 'calc(100vh - 158px)'
-	});
+	
+	$('.slimScrollBar').css({ 'right': 'unset !important', 'left' : 0, });
+	$('.scroolbox').slimScroll({ height: 'auto' });
+	$('.chosen-drop').slimScroll({ height: 'auto' });
+	$('.nofity-list').slimScroll({ height: '200px' });
+	$('.timeline-area').slimScroll({ height: '500px' });
+	$('.recent-activity').slimScroll({ height: 'calc(100vh - 114px)' });
+	$('.settings-list').slimScroll({ height: 'calc(100vh - 158px)' });
 	
 	$(window).on('scroll', function() {
-		var scroll = $(window).scrollTop(), mainHeader = $('#sticky-header'), mainHeaderHeight = mainHeader.innerHeight();
+		var scroll           = $(window).scrollTop(), 
+			mainHeader       = $('#sticky-header'), 
+			mainHeaderHeight = mainHeader.innerHeight();
 
 		if (scroll > 1) {
 			$("#sticky-header").addClass("sticky-menu");
@@ -97,16 +89,6 @@
 			responsive : true
 		});
 	}
-	if ($('#dataTable2').length) {
-		$('#dataTable2').DataTable({
-			responsive : true
-		});
-	}
-	if ($('#dataTable3').length) {
-		$('#dataTable3').DataTable({
-			responsive : true
-		});
-	}
 	
 	$('ul#nav_menu').slicknav({
 		prependTo : "#mobile_menu"
@@ -125,37 +107,6 @@
 		$('.offset-area').toggleClass('show_hide');
 		$('.settings-btn').toggleClass('active');
 	});
-	
-	function slider_area() {
-		var owl = $('.testimonial-carousel').owlCarousel({
-			margin : 50,
-			loop : true,
-			autoplay : false,
-			nav : false,
-			dots : true,
-			responsive : {
-				0 : {
-					items : 1
-				},
-				450 : {
-					items : 1
-				},
-				768 : {
-					items : 2
-				},
-				1000 : {
-					items : 2
-				},
-				1360 : {
-					items : 1
-				},
-				1600 : {
-					items : 2
-				}
-			}
-		});
-	}
-	slider_area();
 	
 	if ($('#full-view').length) {
 
@@ -239,32 +190,8 @@
 	}
 
 	if ($('.page-sound').length) {ion.sound({
-			sounds : [ 
-			/*	{name : "beer_can_opening"}, 
-				{name : "bell_ring", volume : 0.6}, 
-				{name : "branch_break",volume : 0.3}, 
-				{name : "button_click"}, 
-				{name : "button_click_on"}, 
-				{name : "button_push"}, 
-				{name : "button_tiny", volume : 0.6}, 
-				{name : "camera_flashing"}, 
-				{name : "camera_flashing_2", volume : 0.6}, */
-				{name : "cd_tray", volume : 0.6},/* 
-				{name : "computer_error"}, 
-				{name : "door_bell"}, 
-				{name : "door_bump", volume : 0.3}, 
-				{name : "glass"}, 
-				{name : "keyboard_desk"}, 
-				{name : "light_bulb_breaking", volume : 0.6}, 
-				{name : "metal_plate"}, 
-				{name : "metal_plate_2"}, 
-				{name : "pop_cork"}, 
-				{name : "snap"}, 
-				{name : "staple_gun"}, 
-				{name : "tap", volume : 0.6}, 
-				{name : "water_droplet"}, 
-				{name : "water_droplet_2"}, 
-				{name : "water_droplet_3", volume : 0.6} */
+			sounds : [
+				{name : "cd_tray", volume : 0.6}
 			],
 			path : handleBaseURL() + soundsPath,
 			preload : true
@@ -274,26 +201,11 @@
 			ion.sound.play("water_droplet_3");
 		});
 	}
-/*
-	if ($('.page-sound').length) {
-		$('input, textarea').on('input', function() {
-			ion.sound.play("tap");
-		});
-		$('input[type=file]').on('click', function() {
-			ion.sound.play("metal_plate_2");
-		});
-		$('input[type=checkbox], input[type=radio]').on('click', function() {
-			ion.sound.play("button_tiny");
-		});
-		$('select').on('change', function() {
-			ion.sound.play("snap");
-		});
-	}
-*/
+	
 	$('#logout').on('click', function() {
 		ion.sound.play('camera_flashing');
 		bootbox.dialog({
-			message : 'Do you want to exit from Blankon?',
+			message : 'Do you want to exit?',
 			title : 'Logout',
 			className : 'modal-danger modal-center',
 			buttons : {
