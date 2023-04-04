ecdb60ca57ae0b4ff81c6018261c149ad02bc79a
wisnuwidi <wisnuwidi@gmail.com>
2023-04-04 22:30:14 +0700

    Bismillah, Update System @:20230404

--- src/Controllers/Core/Craft/Handler.php
+++ src/Controllers/Core/Craft/Handler.php
@@ -2,7 +2,7 @@
 namespace Incodiy\Codiy\Controllers\Core\Craft;
 /**
  * Created on 2 Apr 2023
- * 
+ *
  * Time Created : 19:50:57
  *
  * @filesource  Handler.php
@@ -16,34 +16,38 @@ trait Handler {
 	private $roleAlias = ['admin'];
 	private $roleInfo  = [];
 	
-	private function roleHandlerInfo($role) {
+	private function roleHandlerInfo($role = []) {
 		$this->roleInfo = $role;
 	}
 	
-	private function roleHandlerAlias($role) {
+	private function roleHandlerAlias($role = []) {
 		$this->roleAlias = $role;
 	}
 	
-	private function initHandler() {}
-	
-	private function customHandler() {}
-	
-	protected function sessionFilters() {
-		$this->initHandler();
-		if ('root' !== $this->session['user_group']) {
-			if (!in_array($this->session['user_group'], $this->roleAlias)) {
-				$this->customHandler();
-				$this->sessionConfig();
+	private function initHandler() {
+		$this->roleHandlerAlias(['admin', 'internal']);
+		$this->roleHandlerInfo(['National']);}
+		
+		private function customHandler() {}
+		
+		protected function sessionFilters() {
+			$this->initHandler();
+			if ('root' !== $this->session['user_group']) {
+				if (!in_array($this->session['user_group'], $this->roleAlias)) {
+					if (!empty($this->roleInfo) && !in_array($this->session['group_alias'], $this->roleInfo)) {
+						$this->customHandler();
+					}
+					$this->sessionConfig();
+				}
 			}
 		}
-	}
-	
-	private function sessionConfig() {
-		$user_session_alias = diy_config('user.alias_session_name');
-		if (!empty($this->session[$user_session_alias])) {
-			foreach ($this->session[$user_session_alias] as $fieldset => $fieldvalues) {
-				$this->filterPage([$fieldset => $fieldvalues], '=');
+		
+		private function sessionConfig() {
+			$user_session_alias = diy_config('user.alias_session_name');
+			if (!empty($this->session[$user_session_alias])) {
+				foreach ($this->session[$user_session_alias] as $fieldset => $fieldvalues) {
+					$this->filterPage([$fieldset => $fieldvalues], '=');
+				}
 			}
 		}
-	}
 }
