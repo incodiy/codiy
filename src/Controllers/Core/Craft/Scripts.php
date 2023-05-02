37481ba0a0abc0474a2e62bba016874105019044
wisnuwidi <wisnuwidi@gmail.com>
2023-05-02 21:49:20 +0700

    Bismillah, Update System @:20230502

--- src/Controllers/Core/Craft/Scripts.php
+++ src/Controllers/Core/Craft/Scripts.php
@@ -40,6 +40,7 @@ trait Scripts {
 						$scripts['js'][] = "vendor/ckeditor-4.10.1/ckeditor.js";
 						$scripts['js'][] = "vendor/ckeditor-4.10.1/config.js";
 						*/
+					    
 						$scripts['js'][] = "vendor/ckeditor/ckeditor.js";
 						$scripts['js'][] = "vendor/ckeditor/config.js";
 					}
