8e42339e38c8bc8604ad42c0a7622c0a0b44ea30
wisnuwidi <wisnuwidi@gmail.com>
2023-09-19 02:56:43 +0700

    Bismillah, Update System @:20230919

--- src/Models/Admin/System/UserActivity.php
+++ src/Models/Admin/System/UserActivity.php
@@ -26,32 +26,29 @@ class UserActivity extends Model {
 	public function montly_activity() {
 		$sql = "
 			SELECT
-				monthly_activity.monthly_activity,
-				d.id group_id,
-				b.id user_id,
-				
-				d.group_info role_group,
-				d.group_alias role_location,
-				
-				b.username,
-				b.fullname,
-				monthly_activity.user_email,
-				
-				monthly_activity.first_access,
-				monthly_activity.last_access,
-				monthly_activity.online_duration,
-				monthly_activity.offline_duration,
-				monthly_activity.offline_duration_day,
-				monthly_activity.login_counters,
-				monthly_activity.logout_counters,
-				monthly_activity.hit_activity,
-				CASE
-					WHEN b.active = 0 THEN 'Disabled'
-					ELSE 'Active'
-				END user_status
+			monthly_activity.monthly_activity,
+			monthly_activity.group_id,
+			monthly_activity.user_id,
+			
+			d.group_info role_group,
+			d.group_alias role_location,
+			
+			b.username,
+			b.fullname,
+			monthly_activity.user_email,
+			
+			monthly_activity.first_access,
+			monthly_activity.last_access,
+			monthly_activity.online_duration,
+			monthly_activity.offline_duration,
+			monthly_activity.login_counters,
+			monthly_activity.logout_counters,
+			monthly_activity.hit_activity,
+			CASE
+				WHEN b.active = 0 THEN 'Disabled'
+				ELSE 'Active'
+			END user_status
 			FROM (
-				
-				# GET MONTHLY DATA ACTIVITY
 				SELECT
 					LEFT(daily_activity, 7) monthly_activity,
 					group_id,
@@ -59,20 +56,17 @@ class UserActivity extends Model {
 					user_email,
 					MIN(first_access) first_access,
 					MAX(last_access) last_access,
-					CONCAT(
+					CONCAT( 
 						FLOOR(TIME_FORMAT(SEC_TO_TIME(SUM(online_duration)), '%H') / 24), ' Days ',
-						FLOOR( MOD(SUM(online_duration), 3600*24) / 3600), 'h ',
-						FLOOR( MOD(SUM(online_duration), 3600) / 60), 'm ',
-						MOD( SUM(online_duration), 60), 's '
-					)
+						FLOOR( MOD(SUM(online_duration), 3600*24) / 3600), ' Hours ',
+						FLOOR( MOD(SUM(online_duration), 3600) / 60), ' Minutes ',
+						MOD( SUM(online_duration), 60), ' Seconds')
 					online_duration,
 					offline_duration,
-					offline_duration_day,
 					SUM(login_counters) login_counters,
 					SUM(logout_counters) logout_counters,
 					SUM(hit_activity) hit_activity
 				FROM (
-				
 					# GET DAILY DATA ACTIVITY
 					SELECT
 						LEFT(a.created_at, 10) daily_activity,
@@ -84,61 +78,51 @@ class UserActivity extends Model {
 						TIMESTAMPDIFF(SECOND, MIN(a.created_at), MAX(a.created_at)) AS online_duration,
 						CASE
 							WHEN LEFT(a.created_at, 10) >= LEFT(NOW(), 7)
-								THEN
+								THEN 
 									CONCAT (
 										TIMESTAMPDIFF(DAY, MAX(a.created_at), NOW()), ' Days, ',
-										FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), NOW()), 3600*24) / 3600), 'h ',
-										FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), NOW()), 3600) / 60), 'm ',
-										FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), NOW()), 60)), 's'
+										FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), NOW()), 3600*24) / 3600), ' Hours, ',
+										FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), NOW()), 3600) / 60), ' Minutes, ',
+										FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), NOW()), 60)), ' Seconds'
 									)
 							ELSE
 								CONCAT (
 									TIMESTAMPDIFF(DAY, MAX(a.created_at), LAST_DAY(LEFT(a.created_at, 10)) ), ' Days, ',
-									FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), CONCAT(LAST_DAY(LEFT(a.created_at, 10)), ' 23:59:59') ), 3600*24) / 3600), 'h ',
-									FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), CONCAT(LAST_DAY(LEFT(a.created_at, 10)), ' 23:59:59') ), 3600) / 60), 'm ',
-									FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), CONCAT(LAST_DAY(LEFT(a.created_at, 10)), ' 23:59:59') ), 60)), 's'
+									FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), CONCAT(LAST_DAY(LEFT(a.created_at, 10)), ' 23:59:59') ), 3600*24) / 3600), ' Hours, ',
+									FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), CONCAT(LAST_DAY(LEFT(a.created_at, 10)), ' 23:59:59') ), 3600) / 60), ' Minutes, ',
+									FLOOR(MOD (TIMESTAMPDIFF(SECOND, MAX(a.created_at), CONCAT(LAST_DAY(LEFT(a.created_at, 10)), ' 23:59:59') ), 60)), ' Seconds'
 								)
 						END offline_duration,
-						CASE
-							WHEN LEFT(a.created_at, 10) >= LEFT(NOW(), 7) THEN TIMESTAMPDIFF(DAY, MAX(a.created_at), NOW())
-							ELSE TIMESTAMPDIFF(DAY, MAX(a.created_at), LAST_DAY(LEFT(a.created_at, 10)) )
-						END offline_duration_day,
 						CONCAT(LAST_DAY(LEFT(a.created_at, 10)), ' 23:59:59') from_last_date,
 						(
-							SELECT COUNT(n.page_info)
-							FROM log_activities n
+							SELECT COUNT(n.page_info) 
+							FROM log_activities n 
 							WHERE n.page_info = 'login_processor'
 								AND LEFT(n.created_at, 10) = LEFT(a.created_at, 10)
-								AND n.user_id = a.user_id
+								AND n.user_id = a.user_id 
 						) login_counters,
 						(
-							SELECT COUNT(n.page_info)
-							FROM log_activities n
+							SELECT COUNT(n.page_info) 
+							FROM log_activities n 
 							WHERE n.page_info = 'logout'
 								AND LEFT(n.created_at, 10) = LEFT(a.created_at, 10)
-								AND n.user_id = a.user_id
+								AND n.user_id = a.user_id 
 						) logout_counters,
 					COUNT(a.page_info) hit_activity
 					FROM `log_activities` a
-					WHERE user_id = 4 AND LEFT(a.created_at, 7) = '2023-06'
+					WHERE user_id > 1 #AND a.page_info IN ('login_processor', 'logout')
 					GROUP BY LEFT(a.created_at, 10), a.user_id, a.user_group_id, a.user_email
 					ORDER BY LEFT(a.created_at, 10) DESC, a.user_id, a.user_group_id
-					# GET DAILY DATA ACTIVITY
-				
 				) monthly
 				GROUP BY LEFT(daily_activity, 7), user_id, group_id, user_email
 				ORDER BY LEFT(daily_activity, 7) DESC, user_id, group_id
-				# GET MONTHLY DATA ACTIVITY
-				
 			) monthly_activity
 			LEFT JOIN mantra_web.users b ON monthly_activity.user_email = b.email
 			LEFT JOIN mantra_web.base_user_group c ON c.user_id = b.id
 			LEFT JOIN mantra_web.base_group d ON c.group_id = d.id AND d.id >= 4
-				
 			WHERE d.group_info IS NOT NULL AND b.username IS NOT NULL
-				
-			GROUP BY LEFT(monthly_activity.monthly_activity, 7), b.id, d.id
-			ORDER BY LEFT(monthly_activity.monthly_activity, 7) DESC, b.id, d.id;
+			GROUP BY LEFT(monthly_activity.monthly_activity, 7), monthly_activity.user_id, monthly_activity.group_id
+			ORDER BY LEFT(monthly_activity.monthly_activity, 7) DESC, monthly_activity.user_id, monthly_activity.group_id
 		";
 		
 		diy_temp_table($this->table, $sql, false);
